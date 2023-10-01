1c96a2f4089c89979e7e52cf66f8596b88033cb0
Ali Abbani <aliabbani08@gmail.com>
2023-10-01 16:54:30 +0300

    up

--- src/app/components/authLinks/AuthLinks.jsx
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import styles from './authLinks.module.css';
+import Link from 'next/link';
 
 const AuthLinks = () => {
+  const status = "ntauthenticated"
   return (
-    <div className={styles.container}>AuthLinks</div>
+    <>
+      {status === "notauthenticated" ? (
+        <Link href="/login">login</Link>
+      ) : (
+        <>
+        <Link href="/write">Write</Link>
+        <span className={styles.link}>Logout</span>
+        </>
+      )
+      }
+    </>
   )
 }
 
