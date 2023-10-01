f2fc9fef5ff581b400bca9a0d8d9f47cd49d3e7f
Ali Abbani <aliabbani08@gmail.com>
2023-10-01 15:54:24 +0300

    add components folder

---
+++ src/app/components/card/card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './card.module.css';
+
+const Card = () => {
+  return (
+    <div className={styles.container}>Card</div>
+  )
+}
+
+export default Card;
---
+++ src/app/components/comments/Comments.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './comments.module.css';
+
+const Comments = () => {
+  return (
+    <div className={styles.container}>Comments</div>
+  )
+}
+
+export default Comments;
