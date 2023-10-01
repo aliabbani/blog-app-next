0131cf67238e6d841d864f11abac18072a96c9fb
Ali Abbani <aliabbani08@gmail.com>
2023-10-01 16:13:48 +0300

    up

--- src/app/components/cardList/CardList.jsx
+++ src/app/components/cardList/CardList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './cardList.module.css';
+import Pagination from '../pagination/Pagination';
 
 const CardList = () => {
   return (
-    <div className={styles.container}>CardList</div>
+    <div className={styles.container}>
+      CardList
+      <Pagination />
+    </div>
   )
 }
 
