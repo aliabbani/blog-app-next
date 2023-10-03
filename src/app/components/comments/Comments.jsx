0063f8cc47c977d10642931915372720243c6438
Ali Abbani <aliabbani08@gmail.com>
2023-10-03 18:50:16 +0300

    add single post page design

--- src/app/components/comments/Comments.jsx
+++ src/app/components/comments/Comments.jsx
@@ -1,10 +1,98 @@
-import React from 'react';
-import styles from './comments.module.css';
+"use client";
+
+import Link from "next/link";
+import styles from "./comments.module.css";
+import Image from "next/image";
+import { useState } from "react";
 
 const Comments = () => {
+  const [desc, setDesc] = useState("");
+  const status = "authenticated";
   return (
-    <div className={styles.container}>Comments</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === "authenticated" ? (
+        <div className={styles.write}>
+          <textarea placeholder="write a comment..." className={styles.input} />
+          <button className={styles.button}>Send</button>
+        </div>
+      ) : (
+        <Link href="/login">Login to write a comment</Link>
+      )}
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>user name</span>
+              <span className={styles.date}>03.10.2023</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries
+          </p>
+        </div>
+      </div>
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>user name</span>
+              <span className={styles.date}>03.10.2023</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries
+          </p>
+        </div>
+      </div>
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>user name</span>
+              <span className={styles.date}>03.10.2023</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Comments;
