a325bc2607bb69cc1445866542b9b5a781028a36
Ali Abbani <aliabbani08@gmail.com>
2023-10-03 17:08:48 +0300

    add single blog page

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,20 @@
+import Menu from "../components/Menu/Menu";
+import CardList from "../components/cardList/CardList";
+import styles from "./blogPage.module.css";
+
+const BlogPage = () => {
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        Blog
+        </h1>
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default BlogPage;
